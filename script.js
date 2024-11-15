3f9f69a14f1d3e7cf8d1cda534b2369b95e3b276
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-11-15 12:03:06 +0530

    updated

--- script.js
+++ script.js
@@ -1,92 +1,80 @@
-
-// let apiUrl = 'https://api.unsplash.com/search/photos '
-// let apiKey = 'jN6ghQlvnd8rB_r_8SCrCfvyYJVxjBhkTtmziI4JVfw'
-
-
-// async function image(params) {
-//     let promise =await fetch(apiUrl, {
-//         method : "GET",
-//         headers : {
-//             'X-Api-Key' : 'jN6ghQlvnd8rB_r_8SCrCfvyYJVxjBhkTtmziI4JVfw'
-            
-//         },
-//         contentType : 'application/json'
-//     });
-
-
-//     return promise;    
-// }
-
-
-
-// image().then((res) =>{
-//     return res.json();
-// }).then((data) =>{
-//     console.log(data);
-// }).catch((error) =>{
-//     console.log(error);
-// });
-
 let count = 1;
-let input = document.querySelector("input")
+let input = document.querySelector("input");
 let btn = document.querySelector(".search");
-let showMore = document.querySelector(".show")
+let showMore = document.querySelector(".show");
 
 let main = document.querySelector("main");
 
-      async function img(params) {
-        let promise = await fetch(`https://api.unsplash.com/search/photos?page=${count}&query=${input.value.toLowerCase()}&client_id=jN6ghQlvnd8rB_r_8SCrCfvyYJVxjBhkTtmziI4JVfw`);
+async function img(params) {
+  let promise = await fetch(
+    `https://api.unsplash.com/search/photos?page=${count}&query=${input.value.toLowerCase()}&client_id=jN6ghQlvnd8rB_r_8SCrCfvyYJVxjBhkTtmziI4JVfw`
+  );
 
-        return promise.json();
-      }
+  return promise.json();
+}
 
-showMore.addEventListener("click" ,()=>{
-    count++;
-    img().then((data) =>{
-        console.log(data);
 
-      }).catch((error) =>{
-        console.log(error);
-      });
+
+btn.addEventListener("click", () => {
+  if (input.value == ""){
+    alert("Enter the image U want !!");
+    return;
+  }
+
+  if (main.childNodes.length > 1){
+    let mainImg = main.querySelectorAll("div");
+      // console.log(mainImg)
+    mainImg.forEach((value) =>{
+
+    value.remove();
+    })
+    
+    }
+
  
-})
-
-btn.addEventListener("click" , ()=>{
-    img().then((data) =>{
-        console.log(data.results);
-        fetchData(data);
-      }).catch((error) =>{
-        console.log(error);
-      });
- 
-   
-    // const apiKey = 'jN6ghQlvnd8rB_r_8SCrCfvyYJVxjBhkTtmziI4JVfw'; // Replace with your API key
-    // const endpoint = 'https://api.unsplash.com/search/photos?query=dog?count=20';
-    // const params = {
-    //     client_id: apiKey,
-    // };
-
-    // fetch(endpoint, { method: 'GET', headers: { 'Authorization': `Client-ID ${apiKey}` }, params })
-    //     .then(response => response.json())
-    //     .then(data => {
-    //        console.log(data.results[0].urls.full);
-    //     })
-    //     .catch(error => console.log('Error:', error));
-})
-
-
-
-function fetchData(arr){
-
-
-arr.forEach((value) =>{
-    console.log(value);
+  img()
+    .then((data) => {
+      fetchData(data.results);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
+function fetchData(arr) {
+
+
+  arr.forEach((element) => {
+    updateOnUI(element);
+  });
+
+  let div = document.createElement("div");
+  div.classList.add("show-more-box");
+  let showMore = document.createElement("button");
+  showMore.classList.add("search");
+  showMore.id = "show-more";
+  showMore.innerText = "Show More";
+  div.appendChild(showMore);
+  main.appendChild(div);
+
+  showMore.addEventListener("click", () => {
+    div.style.display = "none";
+    count++;
+    img()
+      .then((data) => {
+        fetchData(data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
 }
 
-function updateOnUI(value){
+function updateOnUI(value) {
+
+
+  // console.log(value);
   let div = document.createElement("div");
   div.classList.add("img-box");
   div.innerHTML = `
@@ -94,7 +82,41 @@ function updateOnUI(value){
             <div class="img-des">
                 <p>${value.alt_description}</p>
             </div>
-  `
-main.appendChild(div);
+  `;
+
+  div.addEventListener("click" , () =>{
+    let popUp = document.createElement("div");
+    popUp.classList.add("popup");
+    popUp.innerHTML = `
+     <div class="popup-item">
+        <div class="popup-img">
+            <img src="${value.urls.full}">
+        </div>
+        <div class="popup-des">
+            <p>${value.alt_description}</p>
+        </div>
+        <button class="clear">X</button>
+    </div>
+    `
+
+    
+
+    main.appendChild(popUp);
+
+    let clear = document.querySelectorAll(".clear");
+clear.forEach((value) =>{
+  value.addEventListener("click" , ()=>{
+    console.log("hi")
+    popUp.style.visibility = "hidden";
+  })
+})
+
+
+  })
+  main.appendChild(div);
+
 
 }
+
+
+
